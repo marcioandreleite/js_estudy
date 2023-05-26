291ffa1cfc03a305bcc860c32a2a9de8dcfd3af8
marcio <marcioandreleite@gmail.com>
2023-05-26 11:33:25 -0300

    feat : add 0026

---
+++ typescript/src/A0026-getters-settres/A0026-gettres-settres.ts
@@ -0,0 +1,20 @@
+export class Pessoa {
+    constructor(
+        private nome: string,
+        private sobrenome: string,
+        private idade: number,
+        private _cpf: string,
+    ) { }
+
+    set cpf(cpf: string) {
+        this._cpf = cpf
+    }
+
+    get cpf(): string {
+        return this._cpf.replace(/\D/g, '')
+    }
+}
+
+const pessoa = new Pessoa('luis', 'miranda', 30, '000.000.000.00')
+pessoa.cpf = '000.000.000.99'
+console.log(pessoa.cpf)
